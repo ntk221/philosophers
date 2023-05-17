c5ae9612d5d8b3194b5b7c91ded5312ee9fe3fef
ntk221 <nittakdub@gmail.com>
2023-05-17 15:20:22 +0900

    引数を受け取るようにする

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <pthread.h>
+
+# define PHILO_NUM	5
 
 typedef enum {
 	THINKING,
@@ -17,11 +20,38 @@ typedef struct {
 	t_fork *rightFork;
 	int  id;
 	int  time_to_die;
+	int  time_to_eat;
+	int  time_to_sleep;
 	int  eat_count;
 	philo_status status;
 } t_philo;
 
+typedef struct {
+	t_philo	*philo;
+	int	philo_num;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+} t_data;
+
+bool	get_data(t_data *data, char *argv[]) {
+	
+	data->philo = malloc(sizeof(t_philo) *  PHILO_NUM);
+	if (data->philo == NULL)
+		return false;
+
+	data->philo_num = atoi(argv[1]);
+	data->time_to_die = atoi(argv[2]);
+	data->time_to_eat = atoi(argv[3]);
+	data->time_to_sleep = atoi(argv[4]);
+	return true;
+}
+
+
+
 int main(int argc,  char *argv[]) {
+	t_data	data;
+
 	if (argc != 5&& argc != 6) {
 		printf("Error\n");
 		return 1;
@@ -32,5 +62,14 @@ int main(int argc,  char *argv[]) {
 		return 1;
 	}
 
-	return 0
+	if (get_data(&data, argv) == false) {
+		free(data.philo);
+		return (0);
+	}
+
+	printf("%d %d %d %d\n", data.philo_num, data.time_to_die, data.time_to_eat, data.time_to_sleep);
+	
+
+	return 0;
 }
+
